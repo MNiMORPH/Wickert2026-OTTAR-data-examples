c0b2a252dc05bcaa946abf697940237850525aae
Andy Wickert <andy@northernwidget.com>
2026-02-22 16:05:19 -0600

    Best-fitting run with new above-Motherlode data set.

---
+++ DiamondFork/run.1164/run_ottar.py
@@ -0,0 +1,17 @@
+# /usr/bin/python3
+#! /home/awickert/anaconda3/bin/python
+
+"""
+This is meant as a schematic example, using data from the Minnesota River at
+Jordan (MN) stream gauge. Parameters are not calibrated to data, and are not
+intended to fit real river-width measurements.
+"""
+
+import ottar
+
+rw = ottar.RiverWidth.from_yaml('config.yaml')
+
+rw.initialize()
+rw.run()
+rw.finalize()
+
